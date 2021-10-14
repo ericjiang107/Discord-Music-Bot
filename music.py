1776906a3777625152d2274a187ee7e99daae170
Eric Jiang <ericjiang107@gmail.com>
2021-10-14 14:09:31 -0400

    fixed errors

--- music.py
+++ music.py
@@ -9,7 +9,7 @@ class music(commands.Cog):
 
     # adding a "join" command:
     @commands.command()
-    async def join(self, ctx):
+    async def join(self,ctx):
         if ctx.author.voice is None:
             await ctx.send("You're not in a voice channel!")
         voice_channel = ctx.author.voice.channel
@@ -20,7 +20,7 @@ class music(commands.Cog):
 
     # addcing a "disconnect" command:
     @commands.command()
-    async def disconnect(self, ctx):
+    async def disconnect(self,ctx):
         await ctx.voice_client.disconnect()
 
     # playing new song if new song is added:
@@ -31,7 +31,7 @@ class music(commands.Cog):
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         # YDL options for audio format:
         YDL_OPTIONS = {'format': 'bestaudio'}
-        vc = ctx.voie_client
+        vc = ctx.voice_client
 
         # streaming the audio:
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
@@ -42,7 +42,7 @@ class music(commands.Cog):
 
     # adding pause button:
     @commands.command()
-    async def pause(self, ctx):
+    async def pause(self,ctx):
         await ctx.voice_client.pause()
         await ctx.send("Paused ⏸")
 
